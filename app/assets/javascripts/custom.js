7710b9b17d7c29f8e34006590f894517f41b2a7a
Przemek Rogos <przeroog@gmail.com>
2014-11-26 00:09:38 +0100

    css/js sheetmenu

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,47 @@
+function hide(id) {
+  document.getElementById(id).style.display = 'none';
+}
+function totop(thisthing){
+  if (thisthing.parent().hasClass('one')) {
+    return;
+  }
+  else{
+    var marker=false;
+    var depth=['five', 'four', 'three', 'two', 'one'];
+    for (var i=0; i<depth.length; i++){
+      if (marker == false && thisthing.parent().hasClass(depth[i])){
+        marker=true;
+      }
+      else if(marker==true){
+        console.log('.'+depth[i]);
+        $('.'+depth[i]).addClass(depth[i-1]);
+        $('.'+depth[i]).removeClass(depth[i]);
+      }
+    }
+    thisthing.parent().removeClass('two three four five');
+    thisthing.parent().addClass('one');
+    thisthing.parent().css('left', '');
+  };
+};
+function pushOut(thisbutton){
+  var zind = thisbutton.parent().css("z-index");
+  if(zind == 5){
+    return;
+  }
+  else if (zind == 4){
+    thisbutton.parent().css("left", "9%");
+  }
+  else if (zind == 3){
+    thisbutton.parent().css("left", "8.5%");
+  }
+  else if (zind == 2){
+    thisbutton.parent().css("left", "8%");
+  }
+  else if (zind == 1){
+    thisbutton.parent().css("left", "7.5%");
+  };
+};
+function pushIn(thisbutton){
+  var zind = thisbutton.parent().css("z-index");
+    thisbutton.parent().css("left", "");
+}
